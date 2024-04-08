7347e4e3f64831aaa4af79d6a6239fb35db6dbac
Gabriel Afonso dos Santos <162643812+n0lost@users.noreply.github.com>
2024-04-08 10:39:25 -0300

    Update TP02Ex01.java

--- TP02Ex01.java
+++ TP02Ex01.java
@@ -1,1 +1,22 @@
+//Gabriel Afonso dos Santos - CB3026167
 
+package exercicios;
+import java.util.Scanner;
+
+public class TP02Ex01 {
+
+
+    public static void ex01() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Primeiro valor:");
+        double valor1 = scanner.nextDouble();
+
+        double valor2;
+        do {
+            System.out.println("Segundo valor (deve ser maior que o primeiro):");
+            valor2 = scanner.nextDouble();
+        } while (valor2 <= valor1);
+    }
+    
+}
