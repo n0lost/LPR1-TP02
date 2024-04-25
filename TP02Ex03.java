119fd30e74601310864a1d204834b59ea6ae3b22
Gabriel Afonso dos Santos <162643812+n0lost@users.noreply.github.com>
2024-04-25 14:31:02 -0300

    Update TP02Ex03.java

--- TP02Ex03.java
+++ TP02Ex03.java
@@ -1,55 +1,86 @@
 //Gabriel Afonso dos Santos - CB3026167
 
-package exercicios;
 import java.util.Scanner;
 
-public class TP02Ex03 {
-
-    public static void ex03() {
-   
+public class Main {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        boolean continuar = true;
 
-        int N;
-        do {
-            System.out.println("Digite a quantidade de numeros (deve ser positivo e menor que 20):");
-            N = scanner.nextInt();
-        } while (N <= 0 || N >= 20);
-    
-        double[] valores = new double[N];
-        double soma = 0;
-        double maior = Double.MIN_VALUE;
-        double menor = Double.MAX_VALUE;
-        int countPos = 0;
-        int countNeg = 0;
-    
-        for (int i = 0; i < N; i++) {
-            System.out.println("Digite o valor " + (i + 1) + ":");
-            valores[i] = scanner.nextDouble();
-    
-            soma += valores[i];
-            if (valores[i] > maior) {
-                maior = valores[i];
+        while (continuar) {
+            int n = 0;
+            boolean nValido = false;
+
+            while (!nValido) {
+                System.out.print("Informe a quantidade de valores (maior que 0 e menor que 20): ");
+                n = scanner.nextInt();
+                if (n > 0 && n < 20) {
+                    nValido = true;
+                } else {
+                    System.out.println("Erro: O numero deve ser positivo e menor que 20.");
+                }
             }
-            if (valores[i] < menor) {
-                menor = valores[i];
+
+            int[] valores = new int[n];
+            int soma = 0;
+            int maiorValor = Integer.MIN_VALUE;
+            int menorValor = Integer.MAX_VALUE;
+            int positivos = 0;
+            int negativos = 0;
+
+            System.out.println("Digite " + n + " valores:");
+
+            for (int i = 0; i < n; i++) {
+                System.out.print("Valor " + (i + 1) + ": ");
+                int valor = scanner.nextInt();
+
+                valores[i] = valor;
+                soma += valor;
+
+                if (valor > maiorValor) {
+                    maiorValor = valor;
+                }
+
+                if (valor < menorValor) {
+                    menorValor = valor;
+                }
+
+                if (valor > 0) {
+                    positivos++;
+                } else if (valor < 0) {
+                    negativos++;
+                }
             }
-            if (valores[i] > 0) {
-                countPos++;
-            } else if (valores[i] < 0) {
-                countNeg++;
+
+            double media = (double) soma / n;
+            double percentPositivos = (double) positivos / n * 100;
+            double percentNegativos = (double) negativos / n * 100;
+
+            System.out.println("Maior valor: " + maiorValor);
+            System.out.println("Menor valor: " + menorValor);
+            System.out.println("Soma dos valores: " + soma);
+            System.out.println("Media aritmetica: " + media);
+            System.out.println("Porcentagem de valores positivos: " + percentPositivos + "%");
+            System.out.println("Porcentagem de valores negativos: " + percentNegativos + "%");
+
+            boolean respostaValida = false;
+
+            while (!respostaValida) {
+                System.out.print("Deseja executar novamente? (S/N): ");
+                String resposta = scanner.next();
+
+                if (resposta.equalsIgnoreCase("S")) {
+                    respostaValida = true;
+                    continuar = true;
+                } else if (resposta.equalsIgnoreCase("N")) {
+                    respostaValida = true;
+                    continuar = false;
+                } else {
+                    System.out.println("Erro: Resposta invalida. Digite 'S' para sim ou 'N' para nao.");
+                }
             }
         }
-    
-        double media = soma / N;
-        double percPos = (double) countPos / N * 100;
-        double percNeg = (double) countNeg / N * 100;
-    
-        System.out.println("O maior valor e: " + maior);
-        System.out.println("O menor valor e: " + menor);
-        System.out.println("A soma dos valores e: " + soma);
-        System.out.println("A media aritmetica dos valores e: " + media);
-        System.out.println("A porcentagem de valores que são positivos e: " + percPos + "%");
-        System.out.println("A porcentagem de valores que são negativos e: " + percNeg + "%");
+
+        scanner.close();
     }
-    
 }
