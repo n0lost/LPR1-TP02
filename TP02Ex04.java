182513464f0fd9a076ff7818e1fd9d13b8336c16
Gabriel Afonso dos Santos <162643812+n0lost@users.noreply.github.com>
2024-04-25 14:31:39 -0300

    Update TP02Ex04.java

--- TP02Ex04.java
+++ TP02Ex04.java
@@ -1,30 +1,30 @@
 //Gabriel Afonso dos Santos - CB3026167
 
-package exercicios;
 import java.util.Scanner;
 
-public class TP02Ex04 {
-
-    public static void ex04() {
-
+public class Main {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        int[][] matriz = new int[2][3];
 
-        double[][] matriz = new double[2][3];
-    
+        System.out.println("Digite seis valores para preencher uma matriz 2x3:");
+
         for (int i = 0; i < 2; i++) {
             for (int j = 0; j < 3; j++) {
-                System.out.println("Digite o valor da posição [" + i + "][" + j + "]:");
-                matriz[i][j] = scanner.nextDouble();
+                System.out.print("Valor [" + i + "][" + j + "]: ");
+                matriz[i][j] = scanner.nextInt();
             }
         }
-    
-        System.out.println("Os valores da matriz sao:");
+
+        System.out.println("Valores armazenados na matriz 2x3:");
         for (int i = 0; i < 2; i++) {
             for (int j = 0; j < 3; j++) {
                 System.out.print(matriz[i][j] + " ");
             }
             System.out.println();
         }
+
+        scanner.close();
     }
-    
 }
