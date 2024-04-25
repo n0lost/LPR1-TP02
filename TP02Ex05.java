7f2062c4ab08b408a251277fc5937487ba91c4ee
Gabriel Afonso dos Santos <162643812+n0lost@users.noreply.github.com>
2024-04-25 14:32:25 -0300

    Update TP02Ex05.java

--- TP02Ex05.java
+++ TP02Ex05.java
@@ -1,1 +1,30 @@
 //Gabriel Afonso dos Santos - CB3026167
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int[][] matriz = new int[3][2];
+
+        System.out.println("Digite seis valores para preencher uma matriz 3x2:");
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 2; j++) {
+                System.out.print("Valor [" + i + "][" + j + "]: ");
+                matriz[i][j] = scanner.nextInt();
+            }
+        }
+
+        System.out.println("Valores armazenados na matriz 3x2:");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 2; j++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        scanner.close();
+    }
+}
